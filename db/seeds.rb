72372232129d6df2ace8ffd97e0a64c0e374a751
Gauthier Masse <masse.gauthier@gmail.com>
2018-04-30 17:34:12 +0200

    avé l'index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+Movie.create(title: "Les marseillais 3", release_year: 2018, director: "Nabinabila")
+Movie.create(title: "Dikkenek", release_year: 2006, director: "Olivier Van Hoofstadt")
+Movie.create(title: "OSS 117 Le Caire Nid d'Espions", release_year: 2006, director: "Michel Hazanavicius")
+Movie.create(title: "Intouchables", release_year: 2011, director: "Éric Toledano")
+Movie.create(title: "Les mtrois frères", release_year: 1995, director: "Bernard Campan")
